d09fa6d7b45c01aeba1e5878aa2835136268345c
Tony2464 <anthony.fargette@yahoo.fr>
2020-10-17 21:40:30 +0200

    Update installFirefoxScript.sh

--- installFirefoxScript.sh
+++ installFirefoxScript.sh
@@ -1,10 +1,55 @@
 #!/usr/bin/env bash
-#Script to install the latest version of Firefox for Debian-based distros
 
-#Adding debian unstable repo 
-echo "deb http://ftp.us.debian.org/debian/ unstable main contrib non-free"  >> /etc/apt/sources.list
+#Script to install the latest version of Firefox for Debian-based distros
+#Anthony Fargette 2020
+
+sourceFile="/etc/apt/sources.list"
+source="deb http://ftp.us.debian.org/debian/ unstable main contrib non-free"
+installCommand="apt update && apt install firefox -yes"
+removeCommand="apt autoremove firefox-esr"
+bye="Bye bye \( ^ -^)/"
+
+#Asking with y/n answer
+function question()
+{
+    while read -r -p "${1}" answer; do
+        case "$answer" in
+            [yY][eE][sS]|[yY]) 
+                return 0 #yes
+                ;;
+            [nN][oO]|[nN])
+                return 1 #no
+                ;;
+            *)
+                echo 'Type "Yes" or "No"'
+        esac
+    done
+}
 
 #Installing Firefox
-sudo apt update && sudo apt install firefox -yes
+function install(){
+    #Check the presence of the repo
+    if grep -q "$source" $sourceFile; then
+        echo $installCommand
+    else
+        #Adding repo
+        echo $source  >> $sourceFile
+        echo $installCommand
+    fi
+}
 
-echo "Firefox is now installed"
+if question "Do you want to install the latest version of Firefox? [Y/n] "; then
+    echo "Nice \( ^ -^)/"
+    install
+    echo "Firefox is now installed!"
+    if question "Do you want to remove Firefox-ESR? [Y/n] "; then
+        echo $removeCommand
+        echo "Firefox-ESR is now uninstalled!"
+        echo $bye
+    else
+        echo $bye
+    fi
+else
+    echo $bye
+    exit
+fi
